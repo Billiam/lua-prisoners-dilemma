d1a27f91c9d62b269f212bf987b05733af1e356d
Billiam <billiamthesecond@gmail.com>
2016-01-10 17:04:06 -0600

    Fix incorrect test nesting.

--- spec/strategy/meta_strategy_spec.lua
+++ spec/strategy/meta_strategy_spec.lua
@@ -32,22 +32,20 @@ describe('meta strategy', function()
   end)
   
   it("does not call strategies after receiving a response", function()
-    it("passes arguments to all strategies", function()
-      local history = {{action="silent", player_action="confess"}}
+    local history = {{action="silent", player_action="confess"}}
 
-      local spies = {
-        spy.new(function() end),
-        spy.new(function() return 'confess' end),
-        spy.new(function() end),
-      }
+    local spies = {
+      spy.new(function() end),
+      spy.new(function() return 'confess' end),
+      spy.new(function() end),
+    }
 
-      local strategy = MetaStrategy(spies[1], spies[2], spies[3])
+    local strategy = MetaStrategy(spies[1], spies[2], spies[3])
 
-      strategy(2, 50, history)
+    strategy(2, 50, history)
 
-      assert.spy(spies[1]).was_called_with(2, 50, history)
-      assert.spy(spies[2]).was_called_with(2, 50, history)
-      assert.spy(spies[3]).was_not_called()
-    end)
+    assert.spy(spies[1]).was_called_with(2, 50, history)
+    assert.spy(spies[2]).was_called_with(2, 50, history)
+    assert.spy(spies[3]).was_not_called()
   end)
 end)
