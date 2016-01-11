14de028d914fc42798e19464d691a6cace7b3ae9
Billiam <billiamthesecond@gmail.com>
2016-01-10 19:23:51 -0600

    Fix initialization when cli path does not contain a slash.

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
-local path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]]
+local path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]*$]]
+path = path or './'
 package.path = path .."?.lua;".. package.path
 require('config').working_directory = path
 require('bootstrap.init')(unpack(arg))
